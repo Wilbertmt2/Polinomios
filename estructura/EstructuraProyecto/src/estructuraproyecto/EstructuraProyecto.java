12eaad166bdbd72bc2f0f512f947f69ffcaca4bf
Wilbertmt2 <86030151+Wilbertmt2@users.noreply.github.com>
2022-03-06 13:24:25 -0600

    Add files via upload

---
+++ estructura/EstructuraProyecto/src/estructuraproyecto/EstructuraProyecto.java
@@ -0,0 +1,17 @@
+
+package estructuraproyecto;
+
+/**
+ *
+ * @author wilim
+ */
+public class EstructuraProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
